f32c6fbdf5bf47d71db73ec628f62b07d208d31a
Tim <timedwards8@gmail.com>
2017-06-22 15:03:34 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ SECRET_KEY = 'very-obscure-key'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 BINGAPI = 'a7ec019ba2cc4014ace305f9c83354b8'
+save_image_location = os.path.abspath(os.path.dirname(__file__)) + '\\app\\static\\'
