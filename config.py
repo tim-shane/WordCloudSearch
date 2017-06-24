2aaaa5de4b47ccd8697611a8fe1b778b145f1d6f
Tim Edwards <timedwards8@gmail.com>
2017-06-24 17:32:42 -0500

    idiot problem - variable in quotes

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ SECRET_KEY = 'some-key-here'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-BINGAPI = "os.environ['BINGAPIV7']"
+BINGAPI = os.environ['BINGAPIV7']
 save_image_location = os.path.abspath(os.path.dirname(__file__)) + '/app/static/'
